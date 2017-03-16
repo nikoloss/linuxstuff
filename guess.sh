ac0282e84250ec2efe84210a66ee4c5f81146624
Rowland <rowland.lan@163.com>
2017-03-16 20:37:48 +0800

    my first commit

---
+++ guess.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+function guess
+{
+    local dice=$(($RANDOM % 6))
+    local result="you lose!"
+    echo your choice is $1 the dice is $dice
+    if [ $dice -ge 3 -a $1 = "m" ] ;then
+	result="you win!"
+    elif [ $dice -lt 3 -a $1 = "s" ] ;then
+	result="you win!"
+    fi
+    echo $result
+}
+guess $1
---
+++ wallpapaer/changewallpaper.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+function debug
+{
+    #debug函数用来调试，可以直接往标准输出打印也可以echo $@ > xx.log来打日志
+    #发布正式版的时候还可以直接把此函数注释掉，避免打印调试信息
+    echo $@
+}
+
+function downpaper
+{
+    local img=`curl -s http://www.topit.me/tag/PC%E5%A3%81%E7%BA%B8/hot \
+              |grep -oP "http:[^>]*?m.jpg" \
+              |sed -n $[$RANDOM%20+1]p\
+              |sed 's/m.jpg/o.jpg/'`
+    debug start downloading [$img]
+    wget $img -O paper.jpg
+    pcmanfm -w `pwd`/paper.jpg
+}
+
+downpaper
