c5646dee48749ea5c05f311bcf12ea4112288921
Rowland <rowland.lan@163.com>
2017-03-17 21:25:19 +0800

    修改：     changewallpaper.sh

--- wallpapaer/changewallpaper.sh
+++ wallpapaer/changewallpaper.sh
@@ -8,13 +8,41 @@ function debug
 
 function downpaper
 {
-    local img=`curl -s http://www.topit.me/tag/PC%E5%A3%81%E7%BA%B8/hot \
+    local img=`curl -s http://www.topit.me/tag/pc壁纸 \
               |grep -oP "http:[^>]*?m.jpg" \
               |sed -n $[$RANDOM%20+1]p\
               |sed 's/m.jpg/o.jpg/'`
     debug start downloading [$img]
     wget $img -O paper.jpg
-    pcmanfm -w /home/$USER/workspace/linuxstuff/wallpapaer/paper.jpg
+
+}
+
+function widgify
+{
+    local weatherbody=`curl -s www.baidu.com/s?wd=天气`
+    local temp=$(echo $weatherbody \
+		     |grep -oP "twoicon_shishi_title\">[^<]*?<" \
+		     |grep -oP "\d+")
+    local status=$(echo $weatherbody\
+                  |grep -oP "twoicon_shishi_sub\">[^<]*?<"\
+                  |grep -oE ">([^<])*\(")
+    local status=${status:1:-1}   #截取字符串，从第1到倒数第1个字节（去头尾）
+    debug temp=$temp status=$status
+    #开始合图 如果你不知道自己计算机里面有哪些字体
+    #使用convert -list font查看并选择一个字体
+    convert paper.jpg \
+	    -resize 1980x \
+	    -fill skyblue \
+	    -pointsize 60 \
+	    -font 文泉驿等宽正黑 \
+	    -draw "fill-opacity 0.6 roundrectangle 180,150 650,550 10,10" \
+	    -fill white \
+	    -draw "text 200,220 '$temp℃ /$status'" \
+	    -pointsize 40  \
+	    -draw "text 220,300 '`env LC_ALL=en_US.UTF-8 date "+%a %B"`'" dest.jpg
+    #把修改壁纸的语句从downpaper函数挪过来！记住图片名字换成合成图dest而非papaer
+    pcmanfm -w `pwd`/dest.jpg
 }
 
 downpaper
+widgify
