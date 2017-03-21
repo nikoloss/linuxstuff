d345459454cc4bb04b5b4988b3400275c3794421
Rowland <rowland.lan@163.com>
2017-03-21 20:52:56 +0800

    修改：     loofm.sh

--- looplayer/loofm.sh
+++ looplayer/loofm.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-tput smcup
-function drawscreen
-{
-    return
+function key_map {
+    read -sn 1 key
+    echo got $key
 }
+
+
+tput smcup
+
+while true ;do
+    key_map
+done
 
 tput rmcup
