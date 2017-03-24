04d37c263db7e186c91745b98b678516a8b9fa64
Rowland <rowland.lan@163.com>
2017-03-24 22:34:03 +0800

    修改：     loofm.sh

--- looplayer/loofm.sh
+++ looplayer/loofm.sh
@@ -1,79 +1,77 @@
 #!/bin/bash
-declare -a songs
-declare -i curr=0
-declare -i size=0
-declare title=""
 
-BOLD="^[[40;32m"
-REV="\033[31;44m"
+declare -a songs
+declare -i size=0
+declare -i cursor=-1
+declare vol
+declare title
+
+SELECTED="\033[31;44m"
 NORMAL="\033[37;44m"
 
-function draw {
-    for index in ${!songs[@]} ;do
-        if [ ! $index -eq $curr ] ;then
-            echo -e $NORMAL${songs[$index]}$NORMAL
-        else
-            echo -e $REV${songs[$index]}$NORMAL
-        fi
-    done
-}
-
-function apply_key {
-    let curr=($curr+1)%$size
-
-}
-
-function key_react {
-    read -sn 1 key
-    case $key in
-        '>') apply_key next ;;
-        '<') apply_key prev ;;
-        '
-') apply_key prev ;;
-    esac
-}
-
-function help {
-  cat <<END_HELP
-
-
-        $title
-
-
-END_HELP
+function debug {
+    echo `date +%T:` $@ >> luoo.log
 }
 
 function init {
+    vol=$1
     local html=$(curl -s http://www.luoo.net/music/$1)
-    local title_t=$(echo $html \
+    title=$(echo $html \
                     |grep -oP "vol-title\">[^<]+<" \
                     |grep -oP ">[^<]+<")
-    title="<vol.$1>"${title_t:1:-1}
+    title=${title:1:-1}
     local body=$(echo $html \
                 |grep -oP "trackname btn-play\">[^<]+<" \
                 |grep -oP ">[^<]+<")
     while read -r line;
     do
-        songs[$size]="\t[-]"${line:1:-1}
         let size=$size+1
+        songs[$size]=${line:1:-1}
     done<<EOF
     $body
 EOF
 }
 
+function draw {
+    clear
+    cat <<EOF
+
+    <vol.$vol>$title
+
+EOF
+    for index in ${!songs[@]} ;do
+        printf "\t"
+        if [ $cursor -eq $index ] ;then
+            echo -e $SELECTED${songs[$index]}$NORMAL
+        else
+            echo -e $NORMAL${songs[$index]}$NORMAL
+        fi
+    done
+}
+
+function keymap {
+    read -sn 1 key
+    case $key in
+        '>') debug "push >" ;;
+        '<') debug "push <" ;;
+        'p') debug "push play" ;;
+    esac
+}
+
+function quitluoo {
+    tput rmcup
+    clear
+    exit
+}
 
 tput init
 tput smcup
-echo -e "\033[37;44m"
-
-
-init 902
+echo -e $NORMAL
+trap quitluoo INT
+init $1
 while true; do
-    clear
-    help
     draw
-    key_react
+    keymap
 done
-clear
-tput rmcup
+quitluoo
 
